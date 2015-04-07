49fb6859780523eedc250d1a5fe952ec3766a14b
Martin Scebba <mscebba@manas.com.ar>
2015-04-07 16:48:48 -0300

    iLab website

---
+++ javascripts/jquery.validate.messages_es.js
@@ -0,0 +1,23 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: ES
+ */
+jQuery.extend(jQuery.validator.messages, {
+  required: "Este campo es obligatorio.",
+  remote: "Por favor, rellena este campo.",
+  email: "Escribe una dirección de correo válida",
+  url: "Escribe una URL válida.",
+  date: "Escribe una fecha válida.",
+  dateISO: "Escribe una fecha (ISO) válida.",
+  number: "Escribe un número entero válido.",
+  digits: "Escribe sólo dígitos.",
+  creditcard: "Escribe un número de tarjeta válido.",
+  equalTo: "Escribe el mismo valor de nuevo.",
+  accept: "Escribe un valor con una extensión aceptada.",
+  maxlength: jQuery.validator.format("No escribas más de {0} caracteres."),
+  minlength: jQuery.validator.format("No escribas menos de {0} caracteres."),
+  rangelength: jQuery.validator.format("Escribe un valor entre {0} y {1} caracteres."),
+  range: jQuery.validator.format("Escribe un valor entre {0} y {1}."),
+  max: jQuery.validator.format("Escribe un valor menor o igual a {0}."),
+  min: jQuery.validator.format("Escribe un valor mayor o igual a {0}.")
+});
